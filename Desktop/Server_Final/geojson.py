7e1def62ed449cfa8dd7a8c8c4c260aaeac03060
HYoung90 <yrraharry@naver.com>
2025-06-17 16:55:49 +0900

    Ignore telegram_config.env

--- Desktop/Server_Final/geojson.py
+++
@@ -1,4 +0,0 @@
-import geopandas as gpd
-
-shapefile = gpd.read_file(r"E:\논문\On going\auto\shp 파일\LSMD_ADM_SECT_UMD_전북특별자치도\LSMD_ADM_SECT_UMD_52_202504.shp")
-shapefile.to_file(r"E:\논문\On going\auto\shp 파일\전북특별자치도_emd.geojson", driver='GeoJSON')
